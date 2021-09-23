cb98ab4ad08480e9e4b8fe1c890a8ca66c59c88d
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-09-23 15:54:55 +0600

    Task component and font awesome trash icon added

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,13 +1,17 @@
 <template>
     <div v-for="task in tasks" :key="task.id">
-        <h4> {{ task.title }} </h4>
+        <Task :task="task" />
     </div>
 </template>
 
 <script>
+import Task from "./Task.vue"
 
 export default {
     name: "Tasks",
+    components: {
+        Task
+    },
     props: {
         tasks: Array
     }
