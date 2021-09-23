f63a6bc08c28aba920abd2b26bee35873cbc6337
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-09-23 15:40:21 +0600

    Tasks component and showing in the UI by v-for loop

---
+++ src/components/Tasks.vue
@@ -0,0 +1,15 @@
+<template>
+    <div v-for="task in tasks" :key="task.id">
+        <h4> {{ task.title }} </h4>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Tasks",
+    props: {
+        tasks: Array
+    }
+}
+</script>
