90805431e377c777a5986d592b2b268c4cbe612b
bdmostafa <mdmostafaalmahmud@gmail.com>
2021-09-23 15:30:13 +0600

    button and header component

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green" />
+    <Button text="Update Task" color="blue" />
+    <Button text="Delete Task" color="red" />
+  </header>
+</template>
+
+<script>
+import Button from "./Button";
+export default {
+  name: "Header",
+  props: {
+    title: String,
+  },
+  components: {
+    Button,
+  },
+  computed: {
+    homePage() {
+      if (this.$route.path === "/") {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
